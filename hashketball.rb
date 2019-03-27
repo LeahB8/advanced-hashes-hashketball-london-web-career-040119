a823672e731048e8d00f516b99b65c3145f44d8b
Leah Boyd <leahboyd08@gmail.com>
2019-03-27 10:49:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,33 +13,26 @@ def game_hash
             :steals => 3,
             :blocks => 1,
             :slam_dunks => 1  
-            }
           },
-          :player2 => {  
-            :player_name => "Reggie Evans",
+          "Reggie Evans" => {
             :number => 30,
             :shoe => 14,
-            :stats => {
-              :points => 12,
-              :rebounds => 12,
-              :assists => 12,
-              :steals => 12,
-              :blocks => 12,
-              :slam_dunks => 7
-            }
+            :points => 12,
+            :rebounds => 12,
+            :assists => 12,
+            :steals => 12,
+            :blocks => 12,
+            :slam_dunks => 7
           },
-          :player3 => {
-            :player_name => "Brook Lopez",
+          "Brook Lopez" => {
             :number => 11,
             :shoe => 17,
-            :stats => {
-              :points => 17,
-              :rebounds => 19,
-              :assists => 10,
-              :steals => 3,
-              :blocks => 1,
-              :slam_dunks => 15
-            }
+            :points => 17,
+            :rebounds => 19,
+            :assists => 10,
+            :steals => 3,
+            :blocks => 1,
+            :slam_dunks => 15
           },
           :player4 => {
             :player_name => "Mason Plumlee",

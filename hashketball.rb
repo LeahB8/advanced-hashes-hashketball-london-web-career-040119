0fb41d5f2c1917a00fe69d3cffdd7b5bff8bddd8
Leah Boyd <leahboyd08@gmail.com>
2019-03-27 10:47:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,11 +8,11 @@ def game_hash
             :number => 0,
             :shoe => 16,
             :points => 22,
-              :rebounds => 12,
-              :assists => 12,
-              :steals => 3,
-              :blocks => 1,
-              :slam_dunks => 1  
+            :rebounds => 12,
+            :assists => 12,
+            :steals => 3,
+            :blocks => 1,
+            :slam_dunks => 1  
             }
           },
           :player2 => {  

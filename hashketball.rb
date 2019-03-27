47d63e905601c64950ad5c6cb90e8870cdbb4edc
Leah Boyd <leahboyd08@gmail.com>
2019-03-27 10:51:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -70,30 +70,25 @@ def game_hash
             :blocks => 7,
             :slam_dunks => 2
           },
-          :player2 => {
-            :player_name => "Bismak Biyombo",
+          "Bismak Biyombo" => {
             :number => 0,
             :shoe => 16,
-            :stats => {
-              :points => 12,
-              :rebounds => 4,
-              :assists => 7,
-              :steals => 7,
-              :blocks => 15,
-              :slam_dunks => 10
-            }
+            :points => 12,
+            :rebounds => 4,
+            :assists => 7,
+            :steals => 7,
+            :blocks => 15,
+            :slam_dunks => 10
           },
-          :player3 => {
-            :player_name => "DeSagna Diop",
+          "DeSagna Diop" => {
             :number => 2,
             :shoe => 14,
-            :stats => {
-              :points => 24,
-              :rebounds => 12,
-              :assists => 12,
-              :steals => 4,
-              :blocks => 5,
-              :slam_dunks => 5
+            :points => 24,
+            :rebounds => 12,
+            :assists => 12,
+            :steals => 4,
+            :blocks => 5,
+            :slam_dunks => 5
             }
           },
           :player4 => {

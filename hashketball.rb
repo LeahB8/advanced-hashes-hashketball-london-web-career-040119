44f75342c9a17769f43519314215343817b52f76
Leah Boyd <leahboyd08@gmail.com>
2019-03-27 10:45:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,12 +7,12 @@ def game_hash
           "Alan Anderson" => {
             :number => 0,
             :shoe => 16,
-              :points => 22,
+            :points => 22,
               :rebounds => 12,
               :assists => 12,
               :steals => 3,
               :blocks => 1,
-              :slam_dunks => 1
+              :slam_dunks => 1  
             }
           },
           :player2 => {  
